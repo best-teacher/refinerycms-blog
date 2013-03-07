f2ebc432d052d0eff0ab8347665136a4fbbfa9af
tompng <tomoyapenguin@gmail.com>
2013-03-07 15:09:48 +0900

    name fix

--- app/models/refinery/teacher_blog/post.rb
+++ app/models/refinery/teacher_blog/post.rb
@@ -78,7 +78,7 @@ module Refinery
         end
 
         def uncategorized
-          live.includes(:categories).where(Refinery::Categorization.table_name => { :teacher_blog_category_id => nil })
+          live.includes(:categories).where(Refinery::TeacherBlog::Categorization.table_name => { :teacher_blog_category_id => nil })
         end
 
         def next(current_record)
--- db/migrate/20110803223522_create_teacher_blog_structure.rb
+++ db/migrate/20110803223522_create_teacher_blog_structure.rb
@@ -30,12 +30,12 @@ class CreateTeacherBlogStructure < ActiveRecord::Migration
 
     add_index Refinery::TeacherBlog::Category.table_name, :id
 
-    create_table Refinery::Categorization.table_name, :id => true do |t|
+    create_table Refinery::TeacherBlog::Categorization.table_name, :id => true do |t|
       t.integer :teacher_blog_category_id
       t.integer :teacher_blog_post_id
     end
 
-    add_index Refinery::Categorization.table_name, [:teacher_blog_category_id, :teacher_blog_post_id], :name => 'index_teacher_blog_categories_teacher_blog_posts_on_bc_and_bp'
+    add_index Refinery::TeacherBlog::Categorization.table_name, [:teacher_blog_category_id, :teacher_blog_post_id], :name => 'index_teacher_blog_categories_teacher_blog_posts_on_bc_and_bp'
   end
 
   def down
@@ -45,8 +45,8 @@ class CreateTeacherBlogStructure < ActiveRecord::Migration
 
     drop_table Refinery::TeacherBlog::Post.table_name
     drop_table Refinery::TeacherBlog::Comment.table_name
-    drop_table Refinery::Category.table_name
-    drop_table Refinery::Categorization.table_name
+    drop_table Refinery::TeacherBlog::Category.table_name
+    drop_table Refinery::TeacherBlog::Categorization.table_name
   end
 
 end
